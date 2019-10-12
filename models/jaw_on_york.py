7069077967301e0496b9c88e2dc7013efb4a624e
John Qu <Johnqu1982@gmail.com>
2019-10-13 07:40:29 +0800

    got jaw on york curves with classical splines method

--- models/jaw_on_york.py
+++ models/jaw_on_york.py
@@ -1,4 +1,5 @@
-from sympy import symbols, nan, diff, lambdify, Eq
+from sympy import symbols, nan, diff, lambdify, Eq, solve, latex, \
+    Piecewise, plot
 from sympy.abc import x, y
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,6 +15,16 @@ def time_to_degree(time, cycle_time=0.9):
     return np.array(time) * time_to_degree_ratio
 
 
+def move_sympyplot_to_axes(p, ax):
+    backend = p.backend(p)
+    backend.ax = ax
+    backend.process_series()
+    backend.ax.spines['right'].set_color('none')
+    backend.ax.spines['bottom'].set_position('zero')
+    backend.ax.spines['top'].set_color('none')
+    plt.close(backend.fig)
+
+
 class Coefficients(object):
     def __init__(self, piece_id, order=6):
         self.c = []
@@ -26,9 +37,10 @@ class Coefficients(object):
 
 
 class ClassicalSplines(object):
-    def __init__(self, knots, order, pva):
+    def __init__(self, knots, order, pvaj):
         self.pe, self.ve, self.ae, self.je, self.pie = [], [], [], [], []
         self.pf, self.vf, self.af, self.jf, self.pif = [], [], [], [], []
+        self.p, self.v, self.a, self.j, self.pi = [], [], [], [], []
         self.co = []
         self.var = []
         self.knots = knots
@@ -36,10 +48,11 @@ class ClassicalSplines(object):
         self.degree = self.order - 1
         self.exprs = [self.pe, self.ve, self.ae, self.je, self.pie]
         self.funcs = [self.pf, self.vf, self.af, self.jf, self.pif]
+        self.solved_expr = [self.p, self.v, self.a, self.j, self.pi]
         self.equations = []
-        self.pva = pva
-
-    def get_expr(self):
+        self.pvaj = pvaj
+        self.exprs_of_x = [[], [], [], [], []]
+        self.piecewise_of_x = []
         for i in range(1, len(self.knots)):
             self.co.append(Coefficients(i))
             p_i = self.co[-1].c[-1]
@@ -57,45 +70,140 @@ class ClassicalSplines(object):
             self.ae.append(a_i)
             self.je.append(jerk_i)
             self.pie.append(ping_i)
-        return self.exprs
-
-    def get_function(self):
-        exprs = self.get_expr()
         for i in range(len(self.knots) - 1):
-            for j in range(len(exprs)):
-                self.funcs[j].append(lambdify(y, exprs[j][i]))
-        return self.funcs
+            for j in range(len(self.exprs)):
+                self.funcs[j].append(lambdify(y, self.exprs[j][i]))
 
-    def build_boundary_conditions(self):
-        f = self.get_function()
-        for i in range(3):
+    def build_boundary_conditions(self, bc_depth):
+        f = self.funcs
+        for i in range(bc_depth):
             self.equations.append(Eq(f[i][0](self.knots[0]).evalf(),
-                                     self.pva[i][0]))
+                                     self.pvaj[i][0]))
             self.equations.append(Eq(f[i][-1](self.knots[-1]).evalf(),
-                                     self.pva[i][-1]))
+                                     self.pvaj[i][-1]))
 
     def build_interpolation_conditions(self, dict_inter_knots):
         """
         :param dict_inter_knots: {1:[0, 1], 2:[0]}
         :return:
         """
-        f = self.get_function()
+        f = self.funcs
         for i in dict_inter_knots.keys():
             for j in dict_inter_knots[i]:
                 self.equations.append(Eq(f[j][i](self.knots[i]).evalf(),
-                                         self.pva[j][i]))
+                                         self.pvaj[j][i]))
 
-    def build_smoothness_conditions(self, loose_knots_ids):
+    def build_smoothness_conditions(self, loose_knots):
         """
         :param loose_knots_ids: [1]
         :return:
         """
-        f = self.get_function()
+        f = self.funcs
         for i in range(1, len(self.knots) - 1):
-            for j in range(5 if i not in loose_knots_ids else 4):
+            for j in range(5 if i not in loose_knots else 5-loose_knots[i]):
                 self.equations.append(
                     Eq(f[j][i - 1](self.knots[i]).evalf(),
                        f[j][i](self.knots[i]).evalf()))
+
+    def build_expressions(self, solutions, latex_print_out=False):
+        for k in range(5):
+            for i in range(len(self.knots) - 1):
+                self.solved_expr[k].append(self.exprs[k][i].subs(
+                    [(self.co[i].c[j], solutions[self.co[i].c[j]]) for j in
+                     range(order)]))
+        if latex_print_out:
+            for i in range(len(self.knots) - 1):
+                print(latex(self.solved_expr[i]))
+
+    def get_solved_expressions(self):
+        return self.solved_expr
+
+    def shift_curve(self, delta1, delta2):
+        exprs_of_y = self.get_solved_expressions()
+        for k in range(len(exprs_of_y)):
+            self.exprs_of_x[k] += [exprs_of_y[k][i].subs(y, x + delta1)
+                                   for i in range(len(knots) - 1)]
+            self.exprs_of_x[k].append(exprs_of_y[k][0].subs(y, x - delta2))
+
+    def get_exprs_of_x(self):
+        return self.exprs_of_x
+
+    def make_piecewise_curve(self):
+        exprs_of_x = self.get_exprs_of_x()
+        for k in range(5):
+            self.piecewise_of_x.append(Piecewise(
+                (0, x < 0),
+                (exprs_of_x[k][0], x <= degree_to_time(43)),
+                (exprs_of_x[k][1], x <= degree_to_time(90)),
+                (exprs_of_x[k][2], x <= degree_to_time(138)),
+                (0, x <= degree_to_time(330)),
+                (exprs_of_x[k][3], x <= degree_to_time(360)),
+                (0, True)))
+
+    def get_piecewise_of_x(self):
+        return self.piecewise_of_x
+
+    def get_functional_position(self):
+        return lambdify(x, self.get_piecewise_of_x()[0])
+
+    def get_functional_velocity(self):
+        return lambdify(x, self.get_piecewise_of_x()[1])
+
+    def get_functional_acceleration(self):
+        return lambdify(x, self.get_piecewise_of_x()[2])
+
+    def get_functional_jerk(self):
+        return lambdify(x, self.get_piecewise_of_x()[3])
+
+    def plot_numerical(self, num=100):
+        t = np.linspace(0, degree_to_time(360),
+                        num, endpoint=True)
+        degree = time_to_degree(t)
+        position = self.get_functional_position()(t)
+        velocity = self.get_functional_velocity()(t)
+        acceleration = self.get_functional_acceleration()(t)
+        jerk = self.get_functional_jerk()(t)
+        fig = plt.figure(figsize=(15, 12), dpi=80)
+        fig.suptitle('SVAJ curves of Jaw on York, with knots on \n' +
+                     str(time_to_degree(self.knots)-30),
+                     fontsize='xx-large')
+        plt.subplot(4, 1, 1)
+        plt.grid()
+        plt.ylabel("Position (mm)")
+        plt.plot(degree, position,
+                 color="blue", linewidth=3.0, linestyle="-")
+        plt.xlim(0.0, 360.0)
+        plt.xticks(np.linspace(0, 360, 37, endpoint=True))
+        # plt.ylim(min(position)-10, max(position)+10)
+        # plt.yticks(np.linspace(-2000, 2000, 9, endpoint=True))
+        plt.subplot(4, 1, 2)
+        plt.grid()
+        plt.ylabel("Velocity (mm/s)")
+        plt.plot(degree, velocity,
+                 color="blue", linewidth=3.0, linestyle="-")
+        plt.xlim(0.0, 360.0)
+        plt.xticks(np.linspace(0, 360, 37, endpoint=True))
+        # plt.ylim(min(position)-10, max(position)+10)
+        # plt.yticks(np.linspace(-2000, 2000, 9, endpoint=True))
+        plt.subplot(4, 1, 3)
+        plt.grid()
+        plt.ylabel("Acceleration (m/s^3)")
+        plt.plot(degree, acceleration/1000,
+                 color="blue", linewidth=3.0, linestyle="-")
+        plt.xlim(0.0, 360.0)
+        plt.xticks(np.linspace(0, 360, 37, endpoint=True))
+        # plt.ylim(min(position)-10, max(position)+10)
+        # plt.yticks(np.linspace(-2000, 2000, 9, endpoint=True))
+        plt.subplot(4, 1, 4)
+        plt.grid()
+        plt.ylabel("Jerk (m/s^4)")
+        plt.plot(degree, jerk/1000,
+                 color="blue", linewidth=3.0, linestyle="-")
+        plt.xlim(0.0, 360.0)
+        plt.xticks(np.linspace(0, 360, 37, endpoint=True))
+        # plt.ylim(min(position)-10, max(position)+10)
+        # plt.yticks(np.linspace(-2000, 2000, 9, endpoint=True))
+        plt.savefig("svaj_of_jaw_on_york.png", dpi=720)
 
 
 if __name__ == "__main__":
@@ -105,6 +213,43 @@ if __name__ == "__main__":
     positions = np.array([0, -131, -41.1, 0])
     velocities = np.array([0, 0, nan, 0])
     accelerations = np.array([0, nan, nan, 0])
-    pva = [positions, velocities, accelerations]
-    cp = ClassicalSplines(knots, order, pva)
-
+    jerks = np.array([0, nan, nan, 0])
+    pvaj = [positions, velocities, accelerations, jerks]
+    cp = ClassicalSplines(knots, order, pvaj)
+    cp.build_boundary_conditions(bc_depth=3)
+    cp.build_interpolation_conditions({1: [0, 1], 2: [0]})
+    # cp.build_smoothness_conditions({1:2, 2:1})
+    cp.build_smoothness_conditions({1:1})
+    solutions = solve(cp.equations, cp.var)
+    cp.build_expressions(solutions, latex_print_out=False)
+    exprs_of_y = cp.get_solved_expressions()
+    delta1 = degree_to_time(30)
+    delta2 = degree_to_time(330)
+    cp.shift_curve(delta1, delta2)
+    cp.make_piecewise_curve()
+    curves = cp.get_piecewise_of_x()
+    for i in range(len(curves)):
+        print(latex(curves[i]))
+    cp.plot_numerical(num=360)
+    # p1 = plot(curves[0], (x, knots[0], degree_to_time(360)),
+    #           title="Displacement",
+    #           ylabel="(mm)",
+    #           show=False)
+    # p2 = plot(curves[1], (x, knots[0], degree_to_time(360)),
+    #           title="Velocity",
+    #           ylabel="(mm/sec)",
+    #           show=False)
+    # p3 = plot(curves[2], (x, knots[0], degree_to_time(360)),
+    #           title="Acceleration",
+    #           ylabel="(mm/sec^2)",
+    #           show=False)
+    # p4 = plot(curves[3], (x, knots[0], degree_to_time(360)),
+    #           title="Jerk",
+    #           ylabel="(mm/sec^3)",
+    #           show=False)
+    # fig, (ax1, ax2, ax3, ax4) = plt.subplots(nrows=4)
+    # move_sympyplot_to_axes(p1, ax1)
+    # move_sympyplot_to_axes(p2, ax2)
+    # move_sympyplot_to_axes(p3, ax3)
+    # move_sympyplot_to_axes(p4, ax4)
+    # plt.show()
