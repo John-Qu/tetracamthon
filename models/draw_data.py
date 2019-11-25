ec19d80282627bacd7a44bb31cdadd2e133fa4a9
John Qu <Johnqu1982@gmail.com>
2019-11-25 11:48:37 +0800

    fix tetra pak curve annotation point and position

--- models/draw_data.py
+++ models/draw_data.py
@@ -11,9 +11,10 @@ right_york_place, right_jaw_place \
 
 right_york_jerk = np.ones(361)
 right_york_jerk[:360] = np.diff(right_york_acc)
-right_york_jerk[-1] = right_york_jerk[0]  # fill the end point as the same of start
+right_york_jerk[-1] = right_york_jerk[
+    0]  # fill the end point as the same of start
 right_york_jerk = right_york_jerk / (0.9 / 360)
-right_jaw_jerk= np.ones(361)
+right_jaw_jerk = np.ones(361)
 right_jaw_jerk[:360] = np.diff(right_jaw_acc)
 right_jaw_jerk[-1] = right_jaw_jerk[0]  # fill end point
 right_jaw_jerk = right_jaw_jerk / (0.9 / 360)
@@ -23,7 +24,8 @@ left_york_acc = np.hstack((right_york_acc[-181:-1], right_york_acc[:181]))
 left_jaw_acc = np.hstack((right_jaw_acc[-181:-1], right_jaw_acc[:181]))
 left_york_velo = np.hstack((right_york_velo[-181:-1], right_york_velo[:181]))
 left_jaw_velo = np.hstack((right_jaw_velo[-181:-1], right_jaw_velo[:181]))
-left_york_place = np.hstack((right_york_place[-181:-1], right_york_place[:181]))
+left_york_place = np.hstack(
+    (right_york_place[-181:-1], right_york_place[:181]))
 left_jaw_place = np.hstack((right_jaw_place[-181:-1], right_jaw_place[:181]))
 
 # Calculate the relative velocities and placements between york and jaw.
@@ -35,7 +37,8 @@ right_jaw_to_york_acc = right_jaw_acc - right_york_acc
 left_jaw_to_york_acc = left_jaw_acc - left_york_acc
 
 
-def annotate_max_min_part_curve(a, start=0, end=None, col='red', mark_size=50, position=(50, 50), font_size=12):
+def annotate_max_min_part_curve(a, start=0, end=None, col='red', mark_size=50,
+                                position=(50, 50), font_size=12):
     """Mark a scatter on the curve of an array at index with position coordinate.
 
     :param a: array-like object
@@ -54,13 +57,17 @@ def annotate_max_min_part_curve(a, start=0, end=None, col='red', mark_size=50, p
         raise IndexError
     index = f(a[start:end]) + start
     plt.scatter([index, ], [a[index], ], mark_size, color=col)
-    plt.annotate("(" + str(index) + ', '+ str(round(a[index], 1)) + ")",
+    plt.annotate("(" + str(index) + ', ' + str(round(a[index], 1)) + ")",
                  xy=(index, a[index]), xycoords='data',
-                 xytext=position, textcoords='offset points', fontsize=font_size,
-                 arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+                 xytext=position, textcoords='offset points',
+                 fontsize=font_size,
+                 arrowprops=dict(arrowstyle="->",
+                                 connectionstyle="arc3,rad=.2"))
 
 
-def annotate_zero_point_on_curve(a, start=0, end=None, step=1, col='red', mark_size=50, position=(50, 50), font_size=12):
+def annotate_zero_point_on_curve(a, start=0, end=None, step=1, col='red',
+                                 mark_size=50, position=(50, 50),
+                                 font_size=12):
     """Mark a scatter on the curve of an array at index with position coordinate.
 
     :param a: array-like object
@@ -71,114 +78,174 @@ def annotate_zero_point_on_curve(a, start=0, end=None, step=1, col='red', mark_s
     :param font_size: int of the messages' size
     :return: None, effecting the plt object
     """
-    index = np.argmin(np.abs(a[start:end:step]))*(step) + start
+    index = np.argmin(np.abs(a[start:end:step])) * (step) + start
     plt.scatter([index, ], [a[index], ], mark_size, color=col)
-    plt.annotate("(" + str(index) + ', '+ str(round(a[index], 1)) + ")",
+    plt.annotate("(" + str(index) + ', ' + str(round(a[index], 1)) + ")",
                  xy=(index, a[index]), xycoords='data',
-                 xytext=position, textcoords='offset points', fontsize=font_size,
-                 arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+                 xytext=position, textcoords='offset points',
+                 fontsize=font_size,
+                 arrowprops=dict(arrowstyle="->",
+                                 connectionstyle="arc3,rad=.2"))
 
 
 # Create a figure of size 8x6 inches, 80 dots per inch
 fig = plt.figure(figsize=(15, 12), dpi=80)
-fig.suptitle('Tetra Pak A3 Flex @ 8000 p/h \n York and Jaw SVAJ curves @ 0.9s/cycle', fontsize='xx-large')
+fig.suptitle(
+    'Tetra Pak A3 Flex @ 8000 p/h \n York and Jaw SVAJ curves @ 0.9s/cycle',
+    fontsize='xx-large')
 
-# Jerk
-plt.subplot(4, 1, 4)
+plt.subplot(4, 1, 1)
 plt.grid()
-plt.ylabel("Jerk (m/s^3)")
-plt.plot(d, right_york_jerk, color="blue", linewidth=3.0, linestyle="-", label="right york")
-plt.plot(d, right_jaw_jerk, color="green", linewidth=3.0, linestyle="-", label="right jaw")
-# plt.legend(loc='upper right')
+plt.xlabel("Machine Degree")
+plt.ylabel("Position and Distance (mm)")
+plt.plot(d, right_york_place, color="blue", linewidth=3.0, linestyle="-",
+         label="right york")
+plt.plot(d, left_york_place, color="blue", linewidth=1.0, linestyle="--",
+         label="left york")
+plt.plot(d, right_jaw_place, color="green", linewidth=3.0, linestyle="-",
+         label="right jaw")
+plt.plot(d, left_jaw_place, color="green", linewidth=1.0, linestyle="--",
+         label="left jaw")
+plt.plot(d, right_jaw_to_york_place, color="red", linewidth=3.0, linestyle="-",
+         label="right jaw to york")
+plt.plot(d, left_jaw_to_york_place, color="red", linewidth=1.0, linestyle="--",
+         label="left jaw to york")
+index_min_diff_right_york_place_right_jaw_place = np.argmin(
+    right_jaw_to_york_place)
+index_max_right_york_place = np.argmax(right_york_place)
+annotate_max_min_part_curve(right_jaw_to_york_place, 20, 80, col="red",
+                            position=(30, 17))
+annotate_max_min_part_curve(right_york_place, 40, 100, col="blue",
+                            position=(10, -30))
+plt.plot([138, 138], [left_york_place[138], right_york_place[138]],
+         color='blue', linewidth=2, linestyle=":")
+plt.annotate(str(round(right_york_place[138] - left_york_place[138])),
+             xy=(138, (right_york_place[138] + left_york_place[138]) / 2),
+             xycoords='data',
+             xytext=(20, -20), textcoords='offset points', fontsize=12,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+plt.legend(loc='upper right')
 plt.xlim(0.0, 360.0)
 plt.xticks(np.linspace(0, 360, 37, endpoint=True))
-plt.ylim(-2000.0, 2000.0)
-plt.yticks(np.linspace(-2000, 2000, 9, endpoint=True))
-
-# Acceleration
-plt.subplot(4, 1, 3)
-plt.grid()
-plt.ylabel("Acceleration (m/s^2)")
-plt.plot(d, right_york_acc, color="blue", linewidth=3.0, linestyle="-", label="right york")
-plt.plot(d, left_york_acc, color="blue", linewidth=1.0, linestyle="--", label="left york")
-# Plot jaw acceleration with a green continuous line of width 1 (pixels)
-plt.plot(d, right_jaw_acc, color="green", linewidth=3.0, linestyle="-", label="right jaw")
-plt.plot(d, left_jaw_acc, color="green", linewidth=1.0, linestyle="--", label="left jaw")
-plt.plot(d, right_jaw_to_york_acc, color="red", linewidth=3.0, linestyle="-", label="right jaw to york")
-plt.plot(d, left_jaw_to_york_acc, color="red", linewidth=1.0, linestyle="--", label="left jaw")
-annotate_max_min_part_curve(right_york_acc, 0, 20, col="blue", position=(-20,-40))
-annotate_max_min_part_curve(right_jaw_acc, 10, 30, col="green", position=(40,-30))
-annotate_max_min_part_curve(right_york_acc, 60, 80, col="blue", position=(-70,-20))
-annotate_max_min_part_curve(right_jaw_acc, 60, 80, col="green", position=(20,-20))
-annotate_max_min_part_curve(right_york_acc, 90, 100, col="blue", position=(-50,50))
-annotate_max_min_part_curve(right_jaw_acc, 90, 100, col="green", position=(-70,10))
-annotate_max_min_part_curve(right_york_acc, 110, 130, col="blue", position=(20,30))
-annotate_max_min_part_curve(right_jaw_acc, 110, 130, col="green", position=(10,-40))
-annotate_max_min_part_curve(right_jaw_acc, 150, 160, col="green", position=(10,20))
-annotate_max_min_part_curve(right_jaw_acc, 180, 190, col="green", position=(-70,-30))
-annotate_max_min_part_curve(right_jaw_acc, 270, 280, col="green", position=(-70,30))
-annotate_max_min_part_curve(right_jaw_acc, 300, 315, col="green", position=(-70,-30))
-annotate_max_min_part_curve(right_york_acc, 330, 340, col="blue", position=(-50,30))
-annotate_max_min_part_curve(right_jaw_acc, 330, 350, col="green", position=(-50,-30))
-annotate_zero_point_on_curve(right_york_acc, 130, 140, col="blue")
-annotate_zero_point_on_curve(right_jaw_acc, 130, 140, col="green")
-annotate_zero_point_on_curve(left_jaw_acc, 90, 80, step=-1, col="green")
-annotate_zero_point_on_curve(left_jaw_acc, 110, 120, col="green")
-# plt.legend(loc='upper right')
-plt.xlim(0.0, 360.0)
-plt.xticks(np.linspace(0, 360, 37, endpoint=True))
-plt.ylim(-60.0, 60.0)
-plt.yticks(np.linspace(-60, 60, 7, endpoint=True))
+plt.ylim(-200, 500)
+plt.yticks(np.linspace(-200, 500, 8, endpoint=True))
+annotate_zero_point_on_curve(right_jaw_to_york_place, 110, 140, col="red",
+                             position=(10, -30))
 
 # Velocity
 plt.subplot(4, 1, 2)
 plt.grid()
 plt.ylabel("Velocity (mm/s)")
-plt.plot(d, right_york_velo, color="blue", linewidth=3.0, linestyle="-", label="right york")
-plt.plot(d, left_york_velo, color="blue", linewidth=1.0, linestyle="--", label="left york")
-plt.plot(d, right_jaw_velo, color="green", linewidth=3.0, linestyle="-", label="right jaw")
-plt.plot(d, left_jaw_velo, color="green", linewidth=1.0, linestyle="--", label="left jaw")
-plt.plot(d, right_jaw_to_york_velo, color="red", linewidth=3.0, linestyle="-", label="right jaw to york")
-plt.plot(d, left_jaw_to_york_velo, color="red", linewidth=1.0, linestyle="--", label="left jaw to york")
+plt.plot(d, right_york_velo, color="blue", linewidth=3.0, linestyle="-",
+         label="right york")
+plt.plot(d, left_york_velo, color="blue", linewidth=1.0, linestyle="--",
+         label="left york")
+plt.plot(d, right_jaw_velo, color="green", linewidth=3.0, linestyle="-",
+         label="right jaw")
+plt.plot(d, left_jaw_velo, color="green", linewidth=1.0, linestyle="--",
+         label="left jaw")
+plt.plot(d, right_jaw_to_york_velo, color="red", linewidth=3.0, linestyle="-",
+         label="right jaw to york")
+plt.plot(d, left_jaw_to_york_velo, color="red", linewidth=1.0, linestyle="--",
+         label="left jaw to york")
 
-annotate_max_min_part_curve(right_york_velo, 130, 150, col="green", position=(-70,70))
-annotate_max_min_part_curve(right_york_velo, 160, 180, col="green", position=(-20,40))
-annotate_max_min_part_curve(right_york_velo, 180, 280, col="green", position=(-10,-30))
-annotate_max_min_part_curve(right_york_velo, 280, 310, col="green", position=(10,15))
-annotate_max_min_part_curve(right_york_velo, 310, 320, col="green", position=(-50,-30))
+annotate_max_min_part_curve(right_york_velo, 130, 150, col="green",
+                            position=(-70, 70))
+annotate_max_min_part_curve(right_york_velo, 160, 180, col="green",
+                            position=(-20, 30))
+annotate_max_min_part_curve(right_york_velo, 180, 280, col="green",
+                            position=(-10, -30))
+annotate_max_min_part_curve(right_york_velo, 280, 310, col="green",
+                            position=(10, 15))
+annotate_max_min_part_curve(right_york_velo, 310, 320, col="green",
+                            position=(-50, -30))
 annotate_zero_point_on_curve(right_jaw_to_york_velo, 120, 140, col="red")
-
 # plt.legend(loc='upper right')
 plt.xlim(0.0, 360.0)
 plt.xticks(np.linspace(0, 360, 37, endpoint=True))
 plt.ylim(-2000, 4000)
 plt.yticks(np.linspace(-2000, 4000, 7, endpoint=True))
 
-plt.subplot(4, 1, 1)
+# Acceleration
+plt.subplot(4, 1, 3)
 plt.grid()
-plt.xlabel("Machine Degree")
-plt.ylabel("Position and Distance (mm)")
-plt.plot(d, right_york_place, color="blue", linewidth=3.0, linestyle="-", label="right york")
-plt.plot(d, left_york_place, color="blue", linewidth=1.0, linestyle="--", label="left york")
-plt.plot(d, right_jaw_place, color="green", linewidth=3.0, linestyle="-", label="right jaw")
-plt.plot(d, left_jaw_place, color="green", linewidth=1.0, linestyle="--", label="left jaw")
-plt.plot(d, right_jaw_to_york_place, color="red", linewidth=3.0, linestyle="-", label="right jaw to york")
-plt.plot(d, left_jaw_to_york_place, color="red", linewidth=1.0, linestyle="--", label="left jaw to york")
-index_min_diff_right_york_place_right_jaw_place = np.argmin(right_jaw_to_york_place)
-index_max_right_york_place = np.argmax(right_york_place)
-annotate_max_min_part_curve(right_jaw_to_york_place, 20, 80, col="red", position=(30,20))
-annotate_max_min_part_curve(right_york_place, 40, 100, col="blue", position=(10,-40))
-plt.plot([133, 133], [left_york_place[133], right_york_place[133]], color='blue', linewidth=2, linestyle=":")
-plt.annotate(str(round(right_york_place[133] - left_york_place[133])),
-                 xy=(133, (right_york_place[133] + left_york_place[133])/2), xycoords='data',
-                 xytext=(20, -20), textcoords='offset points', fontsize=12,
-                 arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
-plt.legend(loc='upper right')
+plt.ylabel("Acceleration (m/s^2)")
+plt.plot(d, right_york_acc, color="blue", linewidth=3.0, linestyle="-",
+         label="right york")
+plt.plot(d, left_york_acc, color="blue", linewidth=1.0, linestyle="--",
+         label="left york")
+# Plot jaw acceleration with a green continuous line of width 1 (pixels)
+plt.plot(d, right_jaw_acc, color="green", linewidth=3.0, linestyle="-",
+         label="right jaw")
+plt.plot(d, left_jaw_acc, color="green", linewidth=1.0, linestyle="--",
+         label="left jaw")
+plt.plot(d, right_jaw_to_york_acc, color="red", linewidth=3.0, linestyle="-",
+         label="right jaw to york")
+plt.plot(d, left_jaw_to_york_acc, color="red", linewidth=1.0, linestyle="--",
+         label="left jaw")
+annotate_max_min_part_curve(right_york_acc, 0, 20, col="blue",
+                            position=(-20, -40))
+annotate_max_min_part_curve(right_jaw_acc, 10, 30, col="green",
+                            position=(40, -30))
+annotate_max_min_part_curve(right_york_acc, 60, 80, col="blue",
+                            position=(-70, -20))
+annotate_max_min_part_curve(right_jaw_acc, 60, 80, col="green",
+                            position=(20, -20))
+annotate_max_min_part_curve(right_york_acc, 90, 100, col="blue",
+                            position=(-50, 50))
+annotate_max_min_part_curve(right_jaw_acc, 90, 100, col="green",
+                            position=(-70, 10))
+annotate_max_min_part_curve(right_york_acc, 110, 130, col="blue",
+                            position=(20, 30))
+annotate_max_min_part_curve(right_jaw_acc, 110, 130, col="green",
+                            position=(10, -40))
+annotate_max_min_part_curve(right_jaw_acc, 150, 160, col="green",
+                            position=(10, 20))
+annotate_max_min_part_curve(right_jaw_acc, 180, 190, col="green",
+                            position=(-70, -30))
+annotate_max_min_part_curve(right_jaw_acc, 270, 280, col="green",
+                            position=(-70, 30))
+annotate_max_min_part_curve(right_jaw_acc, 300, 315, col="green",
+                            position=(-70, -30))
+annotate_max_min_part_curve(right_york_acc, 330, 340, col="blue",
+                            position=(-50, -40))
+annotate_max_min_part_curve(right_jaw_acc, 330, 350, col="green",
+                            position=(-50, -35))
+annotate_zero_point_on_curve(right_york_acc, 130, 140,
+                             col="blue", position=(-50, 40))
+annotate_zero_point_on_curve(right_jaw_acc, 310, 320,
+                             col="green", position=(0, 20))
+annotate_zero_point_on_curve(right_jaw_acc, 270, 260,
+                             step=-1, col="green", position=(-50, 10))
+annotate_zero_point_on_curve(right_jaw_acc, 290, 300,
+                             col="green", position=(-10, 20))
+annotate_zero_point_on_curve(right_jaw_acc, 190, 200,
+                             col="green", position=(20, 20))
+annotate_zero_point_on_curve(right_jaw_acc, 150, 140,
+                             step=-1, col="green", position=(10, -30))
+annotate_zero_point_on_curve(right_jaw_acc, 160, 180,
+                             col="green", position=(20, 20))
+annotate_zero_point_on_curve(right_york_acc, 320, 330,
+                             col="blue", position=(-20, -30))
+# plt.legend(loc='upper right')
 plt.xlim(0.0, 360.0)
 plt.xticks(np.linspace(0, 360, 37, endpoint=True))
-plt.ylim(-200, 500)
-plt.yticks(np.linspace(-200, 500, 8, endpoint=True))
-annotate_zero_point_on_curve(right_jaw_to_york_place, 110, 140, col="red")
+plt.ylim(-60.0, 60.0)
+plt.yticks(np.linspace(-60, 60, 7, endpoint=True))
+
+# Jerk
+plt.subplot(4, 1, 4)
+plt.grid()
+plt.ylabel("Jerk (m/s^3)")
+plt.plot(d, right_york_jerk, color="blue", linewidth=3.0, linestyle="-",
+         label="right york")
+plt.plot(d, right_jaw_jerk, color="green", linewidth=3.0, linestyle="-",
+         label="right jaw")
+# plt.legend(loc='upper right')
+plt.xlim(0.0, 360.0)
+plt.xticks(np.linspace(0, 360, 37, endpoint=True))
+plt.ylim(-2000.0, 2000.0)
+plt.yticks(np.linspace(-2000, 2000, 9, endpoint=True))
 
 # Save figure using 720 dots per inch
 plt.savefig("Tetra Pak A3 flex Curves.png", dpi=720)
