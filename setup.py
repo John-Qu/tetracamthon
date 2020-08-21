066bf75440ef10d04a5fb3e19540208170f17a0e
John Qu <Johnqu1982@gmail.com>
2020-08-21 11:33:11 +0800

    fill setup.py fully

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from setuptools import setup
 
 setup(
     name='tetracamthon',
+    description="Re-engineering Tetra Pak A3 Flex filling machine's driving " +
+                "cam profile with Python",
+    version='v1.0',
+    author='John Qu',
+    author_email="johnqu1982@gmail.com",
+    url="https://github.com/John-Qu/tetracamthon",
     package=find_package(where='src'),
     package_dir={'': 'src'},
 )
